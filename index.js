4bb232edafd65ed9865ee76232fcad3bff119588
iek30 <132556125+iek30@users.noreply.github.com>
2023-12-05 10:12:13 +0100

    fin

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const { getAll, getPregunta, getPreguntasCategoria, getCategorias } = require('./ServicioTrivial');
+
+corsOptions = {
+  orign:'*',
+  methods:'GET'
+}
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 //-------------------------------------------------------//
 
